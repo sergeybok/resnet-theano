c93f3263b58e992df6f2d8d92654bf433ca8da61
Sergey Bokhnyak <sergeybok@gmail.com>
2017-03-09 14:46:40 -0500

    wrote the build model function, corrected some stuff in resnet-funcs, there's a batch normalization hack involving dimshuffle that I am not proud of but I don't know how to do it another way and no one has responded to me on theano-user group yet

---
+++ resnet.py
@@ -0,0 +1,269 @@
+
+import numpy as np 
+import theano
+from theano import tensor as T 
+from theano import config
+from theano.tensor.signal import pool
+from theano.tensor.nnet import conv2d
+from theano.tensor.nnet.bn import batch_normalization as BN
+
+from resnet_funcs import *
+
+
+
+
+
+lr = np.asarray(0.0001,dtype='float32')
+n_epochs = 10
+
+
+
+
+def build_model(load_params, res_param_names):
+
+	# input assumed normalized(?) with shape (1,3,224,224)	
+
+	X = T.ftensor4('input - X')
+	# target assumed shape (1000,)
+	Y = T.ivector('target - Y')
+	LR = T.fscalar('learning rate')
+	# load theano weights that aren't res params
+	conv1_W = theano.shared(value=np.transpose(load_params['conv1/W'],(3,2,0,1)),borrow=True,name='conv1_W')
+	bn1_conv1_gamma = theano.shared(load_params['bn_conv1/gamma'], borrow=True,name='bn_conv1_gamma')
+	bn1_conv1_beta = theano.shared(load_params['bn_conv1/beta'], borrow=True,name='bn_conv1_beta')
+	bn1_conv1_mean = theano.shared(load_params['bn_conv1/mean/EMA'],borrow=True,name='bn_conv1_mean')
+	bn1_conv1_std = theano.shared(np.sqrt(load_params['bn_conv1/variance/EMA']),borrow=True,name='bn_conv1_std-dev')	
+
+	conv1_params = [conv1_W,bn1_conv1_gamma,bn1_conv1_beta,bn1_conv1_mean,bn1_conv1_std]
+	fc_W = theano.shared(load_params['fc1000/W'],borrow=True,name='fc1000_W')
+	fc_b = theano.shared(load_params['fc1000/b'],borrow=True,name='fc1000_b')
+	fc_params = [fc_W, fc_b]
+	# build first convolution, batchnorm, and pool
+	conv1_out = conv2d(input=X,
+						filters=conv1_W,
+						filter_shape=(64,3,7,7),
+						subsample=(2,2),
+						border_mode=(3,3))
+	layer1_bn_out = T.nnet.relu(BN(inputs=conv1_out.dimshuffle(0,2,3,1),
+								gamma=bn1_conv1_gamma,
+								beta=bn1_conv1_beta, 
+								mean=bn1_conv1_mean,
+								std=bn1_conv1_std))
+	# downsample of size 3 with stride of 2
+	current_output = pool.pool_2d(input=layer1_bn_out.dimshuffle(0,3,1,2),
+								ds=(3,3),
+								st=(2,2),
+								mode='max',
+								ignore_border=False)
+	current_dim = 64
+	# build residual connections
+	# 	first one no stride
+	res_params = []
+	current_output, current_params, current_dim = ResidualUnit(name=res_param_names[0],
+								left_b=current_output,
+								right_b=current_output,
+								load_params=load_params,
+								down_dim=64,
+								up_dim=256,
+								cur_dim=current_dim,
+								stride=(1,1),
+								left_convolve=True)
+	res_params += current_params
+	res_param_names = res_param_names[1:]
+	for name in res_param_names:
+		a = False
+		st = (1,1)
+		if name.endswith('a'):
+			a = True
+			down_dim = current_dim / 2
+			up_dim = down_dim * 4
+			st = (2,2)
+		else:
+			down_dim = current_dim / 4
+			up_dim = down_dim * 4
+		current_output, current_params, current_dim = ResidualUnit(name=name,
+								left_b=current_output,
+								right_b=current_output,
+								load_params=load_params,
+								down_dim=down_dim,
+								up_dim=up_dim,
+								cur_dim=current_dim,
+								stride=st,
+								left_convolve=a)
+		res_params += current_params
+	# pool before fc classifier
+	pool2_out = pool.pool_2d(input=current_output,
+							ds=(7,7),
+							st=(1,1),
+							mode='average_exc_pad',
+							ignore_border=False)
+	# final dense layer
+	fc_out = T.dot(pool2_out.flatten(),fc_W) + fc_b
+	#fc_out = T.dot(fc_W,pool2_out) + fc_b	
+	
+
+	Y_hat = T.nnet.softmax(fc_out.flatten()) # prediction probabilities
+	prediction = T.argmax(Y_hat) # prediction class 
+	params = conv1_params + res_params + fc_params
+	cost = T.nnet.categorical_crossentropy(Y_hat, Y).mean()
+	grads = T.grad(cost,params)
+	# dumb update... optimally sub a better one
+	updates = [(weight, weight - LR*gradient) for weight, gradient in zip(params, grads)]
+	train = theano.function(inputs=[X,Y],outputs=cost,updates=updates,givens={LR:lr})
+	validate = theano.function(inputs=[X,Y],outputs=cost)
+	predict = theano.function(inputs=[X], outputs=prediction)
+
+	return train, validate, predict, params
+
+
+
+
+
+
+print('loading ResNet101.npy file')
+load_params = np.load('ResNet101.npy',encoding='latin1').item()
+
+
+# this naming is taken from the way it is saved in the npy file
+res_param_names = ['2a', '2b', '2c', '3a', '3b1', '3b2', '3b3', '4a', '4b1',
+		 '4b2', '4b3', '4b4', '4b5', '4b6', '4b7', '4b8', '4b9','4b10', 
+		 '4b11', '4b12', '4b13', '4b14', '4b15', '4b16', '4b17',
+		 '4b18', '4b19', '4b20', '4b21', '4b22', '5a', '5b', '5c']
+
+
+
+
+
+print('building model...')
+
+train, validate, predict, params = build_model(load_params, res_param_names)
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+# input assumed normalized(?) with shape (1,3,224,224)
+
+X = T.ftensor4('input - X')
+# target assumed shape (1000,)
+Y = T.ivector('target - Y')
+LR = T.fscalar('learning rate')
+# load theano weights that aren't res params
+conv1_W = theano.shared(value=np.transpose(load_params['conv1/W'],(3,2,0,1)),borrow=True,name='conv1_W')
+bn1_conv1_gamma = theano.shared(load_params['bn_conv1/gamma'], borrow=True,name='bn_conv1_gamma')
+bn1_conv1_beta = theano.shared(load_params['bn_conv1/beta'], borrow=True,name='bn_conv1_beta')
+bn1_conv1_mean = theano.shared(load_params['bn_conv1/mean/EMA'],borrow=True,name='bn_conv1_mean')
+bn1_conv1_std = theano.shared(np.sqrt(load_params['bn_conv1/variance/EMA']),borrow=True,name='bn_conv1_std-dev')
+
+conv1_params = [conv1_W,bn1_conv1_gamma,bn1_conv1_beta,bn1_conv1_mean,bn1_conv1_std]
+fc_W = theano.shared(load_params['fc1000/W'],borrow=True,name='fc1000_W')
+fc_b = theano.shared(load_params['fc1000/b'],borrow=True,name='fc1000_b')
+fc_params = [fc_W, fc_b]
+# build first convolution, batchnorm, and pool
+conv1_out = conv2d(input=X,
+					filters=conv1_W,
+					filter_shape=(64,3,7,7),
+					subsample=(2,2),
+					border_mode=(3,3))
+layer1_bn_out = T.nnet.relu(BN(inputs=conv1_out.dimshuffle(0,2,3,1),
+							gamma=bn1_conv1_gamma,
+							beta=bn1_conv1_beta, 
+							mean=bn1_conv1_mean,
+							std=bn1_conv1_std))
+# downsample of size 3 with stride of 2
+current_output = pool.pool_2d(input=layer1_bn_out.dimshuffle(0,3,1,2),
+							ds=(3,3),
+							st=(2,2),
+							mode='max',
+							ignore_border=False)
+current_dim = 64
+# build residual connections
+# 	first one no stride
+res_params = []
+current_output, current_params, current_dim = ResidualUnit(name=res_param_names[0],
+							left_b=current_output,
+							right_b=current_output,
+							load_params=load_params,
+							down_dim=64,
+							up_dim=256,
+							cur_dim=current_dim,
+							stride=(1,1),
+							left_convolve=True)
+res_params += current_params
+res_param_names = res_param_names[1:]
+for name in res_param_names:
+	a = False
+	st = (1,1)
+	if name.endswith('a'):
+		a = True
+		down_dim = current_dim / 2
+		up_dim = down_dim * 4
+		st = (2,2)
+	else:
+		down_dim = current_dim / 4
+		up_dim = down_dim * 4
+	current_output, current_params, current_dim = ResidualUnit(name=name,
+							left_b=current_output,
+							right_b=current_output,
+							load_params=load_params,
+							down_dim=down_dim,
+							up_dim=up_dim,
+							cur_dim=current_dim,
+							stride=st,
+							left_convolve=a)
+	res_params += current_params
+# pool before fc classifier
+pool2_out = pool.pool_2d(input=current_output,
+						ds=(7,7),
+						st=(1,1),
+						mode='average_exc_pad',
+						ignore_border=False)
+# final dense layer
+fc_out = T.dot(pool2_out.flatten(),fc_W) + fc_b
+#fc_out = T.dot(fc_W,pool2_out) + fc_b
+
+
+Y_hat = T.nnet.softmax(fc_out.flatten()) # prediction probabilities
+prediction = T.argmax(Y_hat) # prediction class 
+params = conv1_params + res_params + fc_params
+cost = T.nnet.categorical_crossentropy(Y_hat, Y).mean()
+grads = T.grad(cost,params)
+# dumb update... optimally sub a better one
+updates = [(weight, weight - LR*gradient) for weight, gradient in zip(params, grads)]
+train = theano.function(inputs=[X,Y],outputs=cost,updates=updates,givens={LR:lr})
+validate = theano.function(inputs=[X,Y],outputs=cost)
+predict = theano.function(inputs=[X], outputs=prediction)
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- resnet_funcs.py
+++ resnet_funcs.py
@@ -3,7 +3,6 @@ import numpy as np
 import theano
 from theano import tensor as T 
 from theano import config
-from theano.tensor.signal import pool
 from theano.tensor.nnet import conv2d
 from theano.tensor.nnet.bn import batch_normalization as BN
 
@@ -12,12 +11,8 @@ from theano.tensor.nnet.bn import batch_normalization as BN
 #X = T.tensor4()
 #n = '2a'
 
-param_names = ['s2a', 's2b', 's2c', 's3a', 's3b1', 's3b2', 's3b3', 's4a', 's4b1',
-		 's4b10', 's4b11', 's4b12', 's4b13', 's4b14', 's4b15', 's4b16', 's4b17',
-		 's4b18', 's4b19', 's4b2', 's4b20', 's4b21', 's4b22', 's4b3', 's4b4',
-		 's4b5', 's4b6', 's4b7', 's4b8', 's4b9', 's5a', 's5b', 's5c']
 
-def ResidualUnit(name,left_b,right_b,load_params,left_convolve=False):
+def ResidualUnit(name,left_b,right_b,load_params,cur_dim,down_dim,up_dim,stride=(1,1),left_convolve=False):
 	# param key names
 	left_res = 'res%s_branch1' % (name)
 	left_bn = 'bn%s_branch1' % (name)
@@ -31,29 +26,29 @@ def ResidualUnit(name,left_b,right_b,load_params,left_convolve=False):
 
 	# init theano weights
 	# NOTE: the np.loaded params do not have any bias vectors 
-	#     except for the final dense layer  
+	#     except for the final dense layer, so no bias additions here
 	params = []
 	if left_convolve:
-		left_res_W = theano.shared(load_params[left_res+'/W'],borrow=True,name=left_res+'_W')
+		left_res_W = theano.shared(value=np.transpose(load_params[left_res+'/W'],(3,2,0,1)),borrow=True,name=left_res+'_W')
 		left_bn_beta = theano.shared(load_params[left_bn+'/beta'],borrow=True,name=left_bn+'_beta')
 		left_bn_gamma = theano.shared(load_params[left_bn+'/gamma'],borrow=True,name=left_bn+'_gamma')
 		left_bn_mean = theano.shared(load_params[left_bn+'/mean/EMA'],borrow=True,name=left_bn+'_mean')
 		left_bn_std = theano.shared(np.sqrt(load_params[left_bn+'/variance/EMA']),borrow=True,name=left_bn+'_std-dev')
 		params += [left_res_W, left_bn_gamma,left_bn_beta,left_bn_mean,left_bn_std]
 	
-	right_res_a_W = theano.shared(load_params[right_res_a+'/W'],borrow=True,name=right_res_a+'_W')
+	right_res_a_W = theano.shared(value=np.transpose(load_params[right_res_a+'/W'],(3,2,0,1)),borrow=True,name=right_res_a+'_W')
 	right_bn_a_beta = theano.shared(load_params[right_bn_a+'/beta'],borrow=True,name=right_bn_a+'_beta')	
 	right_bn_a_gamma = theano.shared(load_params[right_bn_a+'/gamma'],borrow=True,name=right_bn_a+'_gamma')
 	right_bn_a_mean = theano.shared(load_params[right_bn_a+'/mean/EMA'],borrow=True,name=right_bn_a+'_mean')
 	right_bn_a_std = theano.shared(np.sqrt(load_params[right_bn_a+'/variance/EMA']),borrow=True,name=right_bn_a+'_std-dev')
 
-	right_res_b_W = theano.shared(load_params[right_res_b+'/W'],borrow=True,name=right_res_b+'_W')
+	right_res_b_W = theano.shared(value=np.transpose(load_params[right_res_b+'/W'],(3,2,0,1)),borrow=True,name=right_res_b+'_W')
 	right_bn_b_beta = theano.shared(load_params[right_bn_b+'/beta'],borrow=True,name=right_bn_b+'_beta')	
 	right_bn_b_gamma = theano.shared(load_params[right_bn_b+'/gamma'],borrow=True,name=right_bn_b+'_gamma')
 	right_bn_b_mean = theano.shared(load_params[right_bn_b+'/mean/EMA'],borrow=True,name=right_bn_b+'_mean')
 	right_bn_b_std = theano.shared(np.sqrt(load_params[right_bn_b+'/variance/EMA']),borrow=True,name=right_bn_b+'_std-dev')
 
-	right_res_c_W = theano.shared(load_params[right_res_c+'/W'],borrow=True,name=right_res_c+'_W')
+	right_res_c_W = theano.shared(value=np.transpose(load_params[right_res_c+'/W'],(3,2,0,1)),borrow=True,name=right_res_c+'_W')
 	right_bn_c_beta = theano.shared(load_params[right_bn_c+'/beta'],borrow=True,name=right_bn_c+'_beta')	
 	right_bn_c_gamma = theano.shared(load_params[right_bn_c+'/gamma'],borrow=True,name=right_bn_c+'_gamma')
 	right_bn_c_mean = theano.shared(load_params[right_bn_c+'/mean/EMA'],borrow=True,name=right_bn_c+'_mean')
@@ -66,25 +61,25 @@ def ResidualUnit(name,left_b,right_b,load_params,left_convolve=False):
 
 	# make tensor graph
 	if left_convolve:
-		left_conv_out = conv2d(input=left_b,filters=left_res_W,filter_shape=(256,64,1,1))
-		left_out = BN(inputs=left_conv_out,gamma=left_bn_gamma,beta=left_bn_beta,
-					mean=left_bn_mean,std=left_bn_std)
+		left_conv_out = conv2d(input=left_b,filters=left_res_W,filter_shape=(up_dim,cur_dim,1,1),subsample=stride)
+		left_out = BN(inputs=left_conv_out.dimshuffle(0,2,3,1),gamma=left_bn_gamma,beta=left_bn_beta,
+					mean=left_bn_mean,std=left_bn_std).dimshuffle(0,3,1,2)
 	else:
 		left_out = left_b
 
-	right_conv_a_out = conv2d(input=right_b,filters=right_res_a_W,filter_shape=(64,64,1,1))
-	right_a_out = T.nnet.relu(BN(inputs=right_conv_a_out,gamma=right_bn_a_gamma,beta=right_bn_a_beta,
-						mean=right_bn_a_mean,std=right_bn_a_std))
-	right_conv_b_out = conv2d(input=right_a_out,filters=right_res_b_W,filter_shape=(64,64,3,3))
-	right_b_out = T.nnet.relu(BN(inputs=right_conv_b_out,gamma=right_bn_b_gamma,beta=right_bn_b_beta,
-						mean=right_bn_b_mean,std=right_bn_b_std))
-	right_c_conv_out = conv2d(input=right_b_out,filters=right_res_c_W,filter_shape=(256,64,1,1))
-	right_out = BN(inputs=right_c_conv_out,gamma=right_bn_c_gamma,beta=right_bn_c_beta,
-						mean=right_bn_c_mean,std=right_bn_c_std)
+	right_conv_a_out = conv2d(input=right_b,filters=right_res_a_W,filter_shape=(down_dim,cur_dim,1,1),subsample=stride)
+	right_a_out = T.nnet.relu(BN(inputs=right_conv_a_out.dimshuffle(0,2,3,1),gamma=right_bn_a_gamma,beta=right_bn_a_beta,
+						mean=right_bn_a_mean,std=right_bn_a_std).dimshuffle(0,3,1,2))
+	right_conv_b_out = conv2d(input=right_a_out,filters=right_res_b_W,filter_shape=(down_dim,down_dim,3,3),border_mode=(1,1))
+	right_b_out = T.nnet.relu(BN(inputs=right_conv_b_out.dimshuffle(0,2,3,1),gamma=right_bn_b_gamma,beta=right_bn_b_beta,
+						mean=right_bn_b_mean,std=right_bn_b_std).dimshuffle(0,3,1,2))
+	right_c_conv_out = conv2d(input=right_b_out,filters=right_res_c_W,filter_shape=(up_dim,down_dim,1,1))
+	right_out = BN(inputs=right_c_conv_out.dimshuffle(0,2,3,1),gamma=right_bn_c_gamma,beta=right_bn_c_beta,
+						mean=right_bn_c_mean,std=right_bn_c_std).dimshuffle(0,3,1,2)
 
 	output = T.nnet.relu(left_out + right_out)
 
-	return output, params
+	return output, params, 4*down_dim
 
 
 
